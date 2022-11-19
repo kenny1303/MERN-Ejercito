0fa011ea615eb92ce661424dc8ed7c8da55e64ab
kenny1303 <arevalosarmientokenny@gmail.com>
2022-11-19 12:50:14 -0500

    v3

--- backend/index.js
+++ backend/index.js
@@ -22,18 +22,20 @@ app.use(fileupload({
 
 conectarDB();
 // middlewares
-// Se utiliza para realizar la comunicacion entre el servidor del frontend y el backendconst dominiosPermitidos = [process.env.FRONTEND_URL];
-// const corsOptions = {
-//     origin: function(origin, callback) {
-//         if (dominiosPermitidos.indexOf(origin) !== -1) {
-//             // El origen del Request esta permitido
-//             callback(null, true);
-//         } else {
-//             callback(new Error('No permitido por CORS'));
-//         }
-//     }
-// };
-// app.use(cors(corsOptions));
+// Se utiliza para realizar la comunicacion entre el servidor del frontend y el backend 
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
+            // El origen del Request esta permitido
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 // Gestion Usuarios
 app.use('/api/usuarios', usuarioRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+//context 
+import { AuthProvider } from './context/AuthProvider'
+
 // Layout (LayoutAuth)
 import LayoutAuth from './Layout/LayoutAuth'
 // pages (usuarios)
@@ -6,9 +10,12 @@ import Login from './pages/Login'
 import Registro from './pages/usuario/Registro'
 import OlvidePassword from './pages/usuario/OlvidePassword'
 import Confirmar from './pages/usuario/Confirmar'
+
+
 function App() {
   return (
     <Router>
+      <AuthProvider>
       <Routes>
         {/* RUTAS PUBLICAS */}
         <Route path='/' element={<LayoutAuth />}>
@@ -18,7 +25,9 @@ function App() {
           <Route path='confirmar/:id' element={<Confirmar />} />
         </Route>
       </Routes>
+      </AuthProvider>
     </Router>
   )
 }
+
 export default App
